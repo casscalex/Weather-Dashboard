e37a185837dde2653a761b1d7a843af50405fb0c
casscalex <casscalex@outlook.com>
2022-02-08 20:46:35 -0600

    called the api, tryed to query the data, can't get it to work

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-var apiKey = b45490873b196cf6ab84e8cf90940925;
-var currentCity = "";
-var lastCity = "";
+
+// request access to the api
+function getApi() {
+
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=857c5b74ba837e515c35fc82b13067c6"
+
+    fetch(requestUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+}
+
+getApi();
+
+// return values for weather forecast
+var temp = document.querySelector("main.temp");
+var wind = document.querySelector("wind.speed");
+var humid = document.querySelector("main.humidity");
+// var uvi = document.querySelector("")
+console.log(temp);
+
+// search bar functionality
+
+// // curentWeather Function
+function currentWeather() {
+
+    document.getElementById("Temp").innerHTML += 
+        "<h3>Temp:</h3>" + temp;
+
+    document.getElementById("Wind").innerHTML += 
+        "<h3>Wind:</h3>" + wind;
+
+    document.getElementById("Humidity").innerHTML += 
+        "<h3>Humidity:</h3>" + humid;
+
+    document.getElementById("UVI").innerHTML += 
+        "<h3>UV Index:</h3>" + uvi;
+ 
+}
+
+currentWeather();
 
